76a11e8a7aeb8daaf7d11fa476d559884f881981
DoHoangTrung <63000950+DoHoangTrung@users.noreply.github.com>
2021-12-27 23:16:19 +0700

    Add project files.

---
+++ EshopeMvcCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EshopeMvcCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
