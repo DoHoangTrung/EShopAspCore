f7e19fa42668cc62acc1e8cfa092c263d583f5d1
DoHoangTrung <63000950+DoHoangTrung@users.noreply.github.com>
2021-12-28 23:58:53 +0700

    Create entities Config entities using fluent api Migration

---
+++ EshopAspCore.Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using EshopAspCore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EshopAspCore.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Order");
+
+            builder.HasKey(o => o.Id);
+        }
+    }
+}
