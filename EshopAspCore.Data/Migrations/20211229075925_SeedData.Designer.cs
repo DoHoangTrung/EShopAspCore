0508e9a52a62f4835023548ac98e3ea5dbbd722d
DoHoangTrung <63000950+DoHoangTrung@users.noreply.github.com>
2021-12-29 15:07:56 +0700

    Seeding database

--- EshopAspCore.Data/Migrations/20211229075925_SeedData.Designer.cs
+++ EshopAspCore.Data/Migrations/20211229075925_SeedData.Designer.cs
@@ -4,14 +4,16 @@ using EshopAspCore.Data.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EshopAspCore.Data.Migrations
 {
     [DbContext(typeof(EshopDbContext))]
-    partial class EshopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211229075925_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
